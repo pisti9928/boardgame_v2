62602f1f71753b8d78c757c5fea0b6693914e531
Káplár István <pistike.kaplar@gmail.com>
2021-05-15 13:09:34 +0200

    SelectionPhase Enum, selectablePositions Arraylist

--- src/main/java/boardgame/BoardGameController.java
+++ src/main/java/boardgame/BoardGameController.java
@@ -1,4 +1,29 @@
 package boardgame;
 
+import boardgame.model.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardGameController {
+
+    private enum SelectionPhase {
+        SELECT_FROM,
+        SELECT_TO;
+
+        public SelectionPhase alter() {
+            return switch (this) {
+                case SELECT_FROM -> SELECT_TO;
+                case SELECT_TO -> SELECT_FROM;
+            };
+        }
+    }
+
+    private SelectionPhase selectionPhase = SelectionPhase.SELECT_FROM;
+
+    private List<Position> selectablePositions = new ArrayList<>();
+
+    private Position selected;
+
+
 }
