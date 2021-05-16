ac2cc8421b0a431251f98c11227d1f371139098c
Káplár István <pistike.kaplar@gmail.com>
2021-05-16 21:05:44 +0200

    setPlayername 2 ágra lett bontva, 2 paramétert kap

--- src/main/java/boardgame/player/PlayerState.java
+++ src/main/java/boardgame/player/PlayerState.java
@@ -1,9 +1,47 @@
 package boardgame.player;
 
+import boardgame.jdbi.PlayerSet;
+import boardgame.model.BoardGameModel;
+import java.util.Random;
+
 public class PlayerState {
     private static Player nextPlayer;
 
-    public static void setStartingPlayer(int szam){
+    private static PlayerSet playerNameBlue =new PlayerSet();
+    private static PlayerSet playerNameRed=new PlayerSet();
+
+
+    public static void setPlayername(Player playerType, String name){
+        switch (playerType){
+            case PLAYERBLUE:
+            {
+                playerNameBlue.setName(name);
+                break;
+
+            }
+            case PLAYERRED:
+            {
+                playerNameRed.setName(name);
+                break;
+            }
+        }
+    }
+    public static String getRedPlayerName(){
+        return playerNameRed.getName();
+    }
+    public static String getBluePlayerName(){
+        return playerNameBlue.getName();
+    }
+
+    public static void setStartingPlayer(){
+        int szam=0;
+        if(BoardGameModel.elsoLepes)
+        {
+            Random rand = new Random();
+            int n = rand.nextInt(2);
+            n = n + 1;
+            szam = n;
+        }
         if(szam == 1) {
             nextPlayer = Player.PLAYERBLUE;
         }
